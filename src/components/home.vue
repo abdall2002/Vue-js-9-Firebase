ccc007949b5b83a8913ee5d9f48331e5578fe933
unknown <abdallahaliahmed517@gmail.com>
2024-10-08 21:44:09 +0300

    FFF

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,58 +1,28 @@
 <template>
     <div class="row row-cols-1 row-cols-md-2 g-4">
 
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+        <!-- ITEM -->
+        <div class="col" v-for="(notes) in notes" :key="notes.id">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ notes.title }}</h5>
+                    <p class="card-text">{{ notes.description }}</p>
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
 
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
     </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-</div>
 </template>
 
+<script setup>
+import { DB } from '@/firebase/config';
+import { collection, getDocs } from 'firebase/firestore'
+import { ref } from 'vue';
 
-<script>
-    import { ref } from 'vue';
-    import { DB } from '@/firebase/config';
-    import { collection, getDocs, onSnapshot } from 'firebase/firestore';
-
-    const notes = ref([]);
+const notes = ref([]);
 
     //// fetch the notes
-    const notesCollection = collection(DB, 'notes');
+    const notesCollection = collection(DB,'notes');
 
     getDocs(notesCollection)
     .then(snapshot => {
@@ -61,6 +31,5 @@
             documents.push({...doc.data(), id: doc.id})
         })
         notes.value = documents;
-        console.log(documents)
     })
 </script>
